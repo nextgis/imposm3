1ecd017396737e2c626e5ce39a8fefdf877231a4
Dmitry Baryshnikov <bishop.dev@gmail.com>
2020-01-26 04:54:14 +0300

    Fix dump query

--- database/postgis/columns.go
+++ database/postgis/columns.go
@@ -58,7 +58,7 @@ func (t *validatedGeometryType) GeneralizeSQL(colSpec *ColumnSpec, spec *General
 		// TODO return warning earlier
 		log.Printf("[warn] validated_geometry column returns polygon geometries for %s", spec.FullName)
 	}
-	return fmt.Sprintf(`ST_Dump(ST_Buffer(ST_SimplifyPreserveTopology("%s", %f), 0)).geom as "%s"`,
+	return fmt.Sprintf(`(ST_Dump(ST_Buffer(ST_SimplifyPreserveTopology("%s", %f), 0))).geom as "%s"`,
 		colSpec.Name, spec.Tolerance, colSpec.Name,
 	)
 }
